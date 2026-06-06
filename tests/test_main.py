a06759803c08a3f45d6474788eb1d7095d8bf5cc
fritzcrackers <fritz.jacobr@gmail.com>
2026-06-06 18:42:45 -0500

    main tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,26 @@
-# tests/test_main.py
-import pytest
+import argparse
 
-from iot_simulator import main
+from iot_simulator.distributions import Normal
+from iot_simulator.event_emitters import PrintEventEmitter
+from iot_simulator.main import clients_to_generate
+from iot_simulator.producer_factory import ProducerFactory
 
 
-def test_run(capsys: pytest.CaptureFixture[str]) -> None:
-    main.run()
-    captured = capsys.readouterr()
-    assert captured.out == "Hello from base-python-project!\n"
+def test_clients_to_generate():
+    factory = ProducerFactory()
+    emitter = PrintEventEmitter()
+
+    args = argparse.Namespace(
+        dist=[
+            {
+                "create_distribution": "normal",
+                "delay_distribution": "normal",
+                "event_value_distribution": "normal",
+                "clients": 3,
+            }
+        ]
+    )
+
+    clients = clients_to_generate(factory, emitter, args)
+    assert len(clients) == 3
+    assert isinstance(clients[0].event_create_distribution, Normal)
