dc8bc9b693fe401169c9a67d05a2d1ce73d105be
fritzcrackers <fritz.jacobr@gmail.com>
2026-06-06 18:47:41 -0500

    reformatting plus close emitter after loop

--- src/iot_simulator/main.py
+++ src/iot_simulator/main.py
@@ -1,34 +1,40 @@
-import asyncio
 import argparse
+import asyncio
 
-from iot_simulator.distributions import Normal, Exponential, LogNormal
-from iot_simulator.event_emitters import RedisEventEmitter
+from iot_simulator.distributions import Distribution, Exponential, LogNormal, Normal
+from iot_simulator.event_emitters import EventEmitter, RedisEventEmitter
 from iot_simulator.producer_factory import ProducerFactory
-from iot_simulator.producers import IOTProducer
+from iot_simulator.producers import IOTProducer, Producer
 
 
-def clients_to_generate(factory: ProducerFactory, emitter, args: argparse.Namespace):
+def clients_to_generate(
+    factory: ProducerFactory, emitter: EventEmitter, args: argparse.Namespace
+) -> list[Producer]:
     all_clients = []
     for group in args.dist:
+        create_distribution: Distribution
+        delay_distribution: Distribution
+        event_value_distribution: Distribution
+
         if group["create_distribution"] == "normal":
             create_distribution = Normal()
+        elif group["create_distribution"] == "exponential":
+            create_distribution = Exponential()
+        elif group["create_distribution"] == "lognormal":
+            create_distribution = LogNormal()
+
         if group["delay_distribution"] == "normal":
             delay_distribution = Normal()
+        elif group["delay_distribution"] == "exponential":
+            delay_distribution = Exponential()
+        elif group["delay_distribution"] == "lognormal":
+            delay_distribution = LogNormal()
+
         if group["event_value_distribution"] == "normal":
             event_value_distribution = Normal()
-
-        if group["create_distribution"] == "exponential":
-            create_distribution = Exponential()
-        if group["delay_distribution"] == "exponential":
-            delay_distribution = Exponential()
-        if group["event_value_distribution"] == "exponential":
+        elif group["event_value_distribution"] == "exponential":
             event_value_distribution = Exponential()
-
-        if group["create_distribution"] == "lognormal":
-            create_distribution = LogNormal()
-        if group["delay_distribution"] == "lognormal":
-            delay_distribution = LogNormal()
-        if group["event_value_distribution"] == "lognormal":
+        elif group["event_value_distribution"] == "lognormal":
             event_value_distribution = LogNormal()
 
         producers = factory.make_producers(
@@ -43,11 +49,14 @@ def clients_to_generate(factory: ProducerFactory, emitter, args: argparse.Namesp
     return all_clients
 
 
-async def run(args) -> None:
+async def run(args: argparse.Namespace) -> None:
     """Core application logic."""
 
     emitter = RedisEventEmitter()
-    factory = ProducerFactory()
-    producers = clients_to_generate(factory, emitter, args)
-    event_loops = [producer.event_loop() for producer in producers]
-    await asyncio.gather(*event_loops)
+    try:
+        factory = ProducerFactory()
+        producers = clients_to_generate(factory, emitter, args)
+        event_loops = [producer.event_loop() for producer in producers]
+        await asyncio.gather(*event_loops)
+    finally:
+        await emitter.close()
